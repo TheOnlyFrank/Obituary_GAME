c899d8a71660e919ac3ca78b85398d45a129d912
FrenchestBaguette <CharlieBell370@gmail.com>
2025-05-01 11:56:37 +0800

    trying to do inventory

--- Obituary_GAME/Assets/InventoryItem.cs
+++ Obituary_GAME/Assets/InventoryItem.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class InventoryItem : MonoBehaviour
+using UnityEngine.EventSystems;
+public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin drag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging");
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End Drag");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
