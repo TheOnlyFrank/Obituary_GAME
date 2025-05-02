2f7c3bdb2abdcb18efbcd5f8d19e8be0a1ca9f24
Andrew Sinclair <falloftheviii@gmail.com>
2025-05-02 12:03:08 +0800

    Coding update for pathfinding

--- Obituary_GAME/Assets/Scenes/Scripts/Enemy_Navigation.cs
+++ Obituary_GAME/Assets/Scenes/Scripts/Enemy_Navigation.cs
@@ -7,15 +7,28 @@ public class Enemy_Navigation : MonoBehaviour
 {
 
     public Transform goal;
+
+    public float shootRange = 4f; 
     public float meleeRange = 1.5f;
     public float rotationSpeed = 10f;
+
+    public NavMeshAgent navAgent; 
 
     
     void Update()
     {
-     //   Transform goal = null;
+        //Transform goal = null;
+       
+        
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
         MoveTowards(goal); 
+        float distance = Vector3.Distance(transform.position, goal.position);
+
+        if (distance <= shootRange)
+        {
+            navAgent.speed = 0f; 
+            RotateTowards(goal);
+        }
         
          //Rotates enemy toward player if within Nav Mesh Agent's stopping distance, so that enemies don't get stuck
          //near the player but looking the wrong way, always turning toward player rather than stopping entirely
@@ -43,13 +56,13 @@ public class Enemy_Navigation : MonoBehaviour
     }
 
         //Rotates enemy toward player even when movement is stopped
-    //private void RotateTowards(Transform goal)
-    //{
-    //    transform.LookAt(goal.transform);
-    //    Vector3 direction = (goal.position - transform.position).normalized;
-    //    Quaternion lookRotation = Quaternion.LookRotation(direction);
-    //    transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
-    //}
+   private void RotateTowards(Transform goal)
+   {
+       transform.LookAt(goal.transform);
+       Vector3 direction = (goal.position - transform.position).normalized;
+       Quaternion lookRotation = Quaternion.LookRotation(direction);
+       transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+   }
 
 
 }
