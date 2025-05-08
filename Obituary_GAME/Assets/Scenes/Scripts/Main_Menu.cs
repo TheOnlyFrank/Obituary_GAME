3ab0d269d99604def90290ff3bf85223f89a7272
TheOnlyFrank <falloftheviii@gmail.com>
2025-05-08 09:18:19 +0800

    Added Menu Screens, starting game initialisation

---
+++ Obituary_GAME/Assets/Scenes/Scripts/Main_Menu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject CreditsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuButton();
+    }
+
+    public void NewGameButton()
+    {
+        // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //public void ContinueButton()
+    //{
+    //    // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
+    //    UnityEngine.SceneManagement.SceneManager.LoadScene("Game_Scene");
+    //}
+
+    public void CreditsButton()
+    {
+        // Show Credits Menu
+        MainMenu.SetActive(false);
+        CreditsMenu.SetActive(true);
+    }
+
+    public void MainMenuButton()
+    {
+        // Show Main Menu
+        MainMenu.SetActive(true);
+        CreditsMenu.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+        // Quit Game
+        Application.Quit();
+    }
+}
