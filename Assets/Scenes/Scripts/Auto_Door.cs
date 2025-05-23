e751c1ebdfae58b8c6b1d2f0f10c16c5e701ed07
LanaB0nana <formercvcboiii1780@gmail.com>
2025-05-23 13:10:22 +0800

    Got some things done
    Trigger works, EPrompt disappears after interact
    Auto door works kinda
    Civ_Cryo_Door opens well
    Halway_Door_2 opens well

--- Assets/Scenes/Scripts/Auto_Door.cs
+++ Assets/Scenes/Scripts/Auto_Door.cs
@@ -15,10 +15,10 @@ public class Auto_Door : MonoBehaviour
     public float delay = 0.0f;
 
     // Start is called before the first frame update
- //   void Start()
- //   {
- //       startPos = transform.position;
- //   }
+    void Start()
+    {
+        startPos = transform.position;
+    }
 
     // Update is called once per frame
     void Update()
